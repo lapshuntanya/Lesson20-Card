a5308e12ee485ddd4677a048279c45eb6abef273
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-20 17:10:29 +0300

    definition Card

--- Lesson20-Card/Card.cpp
+++ Lesson20-Card/Card.cpp
@@ -1,1 +1,34 @@
 #include "Card.h"
+
+Card::Card(){
+	cardNumber = rand() * pow(10, 10) + rand();
+	strcpy_s(name, 50, "undefined");
+	strcpy_s(expiredDate, 6, "01/34");
+	PIN = rand() % 9999 + 1000;
+	cash = 0;
+	cout << "Opened STANDART card: " << cardNumber << endl;
+}
+
+Card::Card(const char* name, double cash){
+	cardNumber = rand() * pow(10, 10) + rand();
+	strcpy_s(this->name, 50, name);//!!!!
+	strcpy_s(expiredDate, 6, "01/34");
+	PIN = rand() % 9999 + 1000;
+	this->cash = cash;//!!!
+	cout << "Opened VIP card: " << cardNumber << endl;//!!!
+}
+
+Card::~Card(){
+	cout << "Closed " << cardNumber << " card\n";
+}
+
+void Card::showInfo(){
+	cout << "++++++++++++++++++++++++++++++++++++++++++++\n";
+	cout << "| \t Card:     " << cardNumber << endl;
+	cout << "| \t Client:   " << name << endl;
+	cout << "| \t Exp date: " << expiredDate << endl;
+	cout << "++++++++++++++++++++++++++++++++++++++++++++\n";
+	cout << "| \t PIN:      " << PIN << endl;
+	cout << "| \t Cash:     " << cash << " UAH" << endl;
+	cout << "++++++++++++++++++++++++++++++++++++++++++++\n";
+}
--- Lesson20-Card/Lesson20-Card.cpp
+++ Lesson20-Card/Lesson20-Card.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     Card mono;
-    mono.
+    mono.showInfo();
 }
 
