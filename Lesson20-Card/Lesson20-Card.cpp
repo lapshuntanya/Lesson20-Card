41bc5e10e070221e7319fc75865c727390d9fff8
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-20 17:16:06 +0300

    main

--- Lesson20-Card/Lesson20-Card.cpp
+++ Lesson20-Card/Lesson20-Card.cpp
@@ -1,8 +1,22 @@
 #include "Card.h"
+#include <ctime>
 
 int main()
 {
-    Card mono;
-    mono.showInfo();
+    srand(time(0));
+
+    Card* mono = nullptr;
+    bool user;
+    cout << "0 - standart card\n";
+    cout << "1 - VIP card\n";
+    cin >> user;
+
+    if (user == 0) mono = new Card();
+    else mono = new Card("Tetiana Lapshun", 25'000);
+
+
+    mono->showInfo();
+
+    delete mono;
 }
 
